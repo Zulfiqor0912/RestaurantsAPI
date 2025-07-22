225d7f9687cc9531c7116dee549fa68bda596ce3
Zulfiqor0912 <zulfiqorrustamov81@gmail.com>
2025-07-22 21:53:04 +0500

    added other function of dishController in dishcontrollerClass

--- RestaurantSolution/Restaurants.Infrastructure/obj/Debug/net8.0/Restaurants.Infrastructure.AssemblyInfo.cs
+++ RestaurantSolution/Restaurants.Infrastructure/obj/Debug/net8.0/Restaurants.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurants.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+200e34d9947a42fdd460c4439c68134618b93129")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c3fb26034fb974482075f63f66a9864927718cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurants.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurants.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
